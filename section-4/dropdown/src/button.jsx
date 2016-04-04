4d40610044abacc10d1ce02b2858814269885b45
Aaron <aaronmagi@gmail.com>
2016-04-03 20:39:01 -0700

    Lecture 21 end

--- section-4/dropdown/src/button.jsx
+++ section-4/dropdown/src/button.jsx
@@ -2,7 +2,8 @@ var React = require('react');
 
 module.exports = React.createClass({
   render: function() {
-    return <button className={"btn " + this.props.className} type="button">
+ // when this.handleClick is called it goes to look for a function inside the class to run
+    return <button onClick={this.props.whenClicked} className={"btn " + this.props.className} type="button">
       {this.props.title}
       <span className={this.props.subTitleClassName}>{this.props.subTitle}</span>
     </button>
--- section-4/dropdown/src/dropdown.jsx
+++ section-4/dropdown/src/dropdown.jsx
@@ -4,13 +4,26 @@
 
 var React = require('react');
 var Button = require('./button');
-//var List = require('./list');
+var ListItem = require('./list-item');
 
 module.exports = React.createClass({
+  handleClick: function() {
+    alert('hello from dropdown');
+  },
 
   render: function () {
+    var list = this.props.items.map(function(item){
+      return <ListItem item={item} />
+    });
+
     return <div className="dropdown">
-      <Button className="btn-default" title={this.props.title} subTitleClassName="caret"/>
+      <Button
+        whenClicked={this.handleClick}
+        className="btn-default"
+        title={this.props.title}/>
+      <ul>
+        {list}
+      </ul>
     </div>
   }
 });
